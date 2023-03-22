99163623963e8a2ad92766d9b2f7e4a89c099742
shizzeer <chat00@wp.pl>
2023-03-22 19:10:42 +0100

    Index static view done

---
+++ frontend/src/indexMainSection.js
@@ -0,0 +1,17 @@
+function IndexMainSection() {
+    return (
+        <div className="imgContainer">
+            <img src="./img/girl-pic.jpg"></img>
+            <div className="textOverlay">
+                <div className="textHeader">Buy, sell and enjoy fashion</div>
+                <div className="imgTxtSection d-none d-lg-block">
+                    Sign Up and join other people on the social marketplace
+                    where everyone can find something that will really love!
+                </div>
+                <div className="imgTxtFooter">Sign Up with Email</div>
+            </div>
+        </div>
+    )
+}
+
+export default IndexMainSection;
