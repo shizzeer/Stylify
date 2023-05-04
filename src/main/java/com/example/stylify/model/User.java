e98cea21657fb48da6cea48388b829104af6c370
Kamil Szpakowski <ksz@MacBook-Pro-Kamil.local>
2023-05-04 15:16:47 +0200

    Marked email and username as unique inside entity

--- src/main/java/com/example/stylify/model/User.java
+++ src/main/java/com/example/stylify/model/User.java
@@ -18,10 +18,10 @@ public class User {
     @OneToMany(mappedBy = "user")
     private Set<Product> products;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "username", nullable = false)
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
 
     @Column(name = "password", nullable = false)
