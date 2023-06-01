61e675b0eacd3ede5408df322d4b8ff8be799adf
Kamil Szpakowski <ksz@MacBook-Pro-Kamil.local>
2023-06-01 18:22:16 +0200

    Responsive menu added

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -40,20 +40,17 @@ function Navbar() {
                         </Link>
                     </div>
 
-                    {/*<div className="collapse navbar-collapse" id="navbarSupportedContent">*/}
-                    {/*    <div className="mr-auto"></div>*/}
-                    {/*    <ul className="navbar-nav my-2 my-lg-0">*/}
-                    {/*        <li className="nav-item active">*/}
-                    {/*            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>*/}
-                    {/*        </li>*/}
-                    {/*        <li className="nav-item dropdown">*/}
-                    {/*            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"*/}
-                    {/*               data-display="static" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">*/}
-                    {/*                Profile*/}
-                    {/*            </a>*/}
-                    {/*        </li>*/}
-                    {/*    </ul>*/}
-                    {/*</div>*/}
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div className="mr-auto"></div>
+                        <ul className="navbar-nav my-2 my-lg-0">
+                            <li className="nav-item active d-md-none"> {/* d-md-none ukrywa element na urządzeniach średnich i większych */}
+                                <Link className="nav-link" to="/sell">Sell <span className="sr-only">(current)</span></Link>
+                            </li>
+                            <li className="nav-item active d-md-none"> {/* d-md-none ukrywa element na urządzeniach średnich i większych */}
+                                <Link className="nav-link" onClick={handleLogout}>Logout <span className="sr-only">(current)</span></Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
             <nav id="searchNav" className="navbar navbar-expand-lg navbar-light bg-light bg-white">
